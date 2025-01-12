ee4e1f6d898df9b6d825396c77bb4ebf7ce57a0f
Rishikesh Suvarna <rishikeshsuvarna@gmail.com>
2025-01-12 20:13:24 +0000

    feat: Add Jest configuration and initial test setup for client application

---
+++ client/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testEnvironment: 'jest-environment-jsdom',
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  };
