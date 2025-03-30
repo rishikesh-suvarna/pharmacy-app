da6b5da727a7908460a964943b28968f251c0c5c
Rishikesh Suvarna <rishikeshsuvarna@gmail.com>
2025-03-30 23:38:30 +0100

    pre-rebuild

--- client/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    testEnvironment: 'jest-environment-jsdom',
-    transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  };
